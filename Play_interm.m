d9e2f9012b0c1294a719ae25df0b964d5f053bd6
kezhili <kezhi.li@imperial.ac.uk>
2017-03-02 17:56:00 +0000

    add play interm
    see the inter-processing states and memoery

--- Play_interm.m
+++ Play_interm.m
@@ -3,29 +3,37 @@
 % 
 % 
 % 
-% 
+%  14/02/2017 Kezhi Li @Imperial College, CSC, MRC
 
 % strain name
 stain =  'N2';
 cur_folder = ['Z:\DLWeights\eig_catagory_Straits\',stain,'\'];
 % the hdf5 file storages the c matrices
-name = 'interm_N2 on food L_2011_02_17__12_51_07___7___7_eig_7-260-260-260-260-7_600ep.hdf5';
-feature_name = 'N2 on food L_2011_02_17__12_51_07___7___7_features.hdf5';
+strain_name =  'N2 on food L_2011_03_17__11_34_06___7___3';
+%'N2 on food L_2011_02_17__12_51_07___7___7';
+name = ['interm_',strain_name,'_eig_7-260-260-260-260-7_600ep.hdf5'];
+feature_name = [strain_name,'_features.hdf5'];
 hdf5_file_name = [cur_folder,name];
 
 
 idx_layer = 1;
-idx_neuron = 1;
+idx_neuron = 226 ;
+idx_feature = 12;
 % the index of layer and neuron one wants to observe
 idx = [idx_layer, idx_neuron];
 
 % c matrix of different layers
-c_L = cell(4);
+c_L = cell(4,1);
+res_L = cell(4,1);
 y_test = h5read(hdf5_file_name,'/y_test'); 
 c_L{1} = h5read(hdf5_file_name,'/c_L0_col'); 
 c_L{2} = h5read(hdf5_file_name,'/c_L1_col'); 
 c_L{3} = h5read(hdf5_file_name,'/c_L3_col'); 
 c_L{4} = h5read(hdf5_file_name,'/c_L5_col'); 
+res_L{1} = h5read(hdf5_file_name,'/res_L0_col'); 
+res_L{2} = h5read(hdf5_file_name,'/res_L1_col'); 
+res_L{3} = h5read(hdf5_file_name,'/res_L3_col'); 
+res_L{4} = h5read(hdf5_file_name,'/res_L5_col'); 
 eig_vec = h5read(hdf5_file_name,'/eig_vec'); 
 len_vec = h5read(hdf5_file_name,'/len_vec'); 
 
@@ -106,55 +114,128 @@ for nf = 1  %num_csv;  % 476
     [Xrecon, Yrecon] = addRBMRotMat(Xtil, Ytil, XCMrecon, YCMrecon, ...
         THETArecon, XCM, YCM, THETA);
     
-    %% plot a movie of worm with motion back in
-    for ii = 1:1:size(Xrecon,1) % only plotting every 10 frames to speed up movie
-%   
-        subplot(5,4,[13,14])
-        imshow(reshape(c_L{1}(:,ii),13,20))
-        caxis([-3,3])
-        colorbar
-        subplot(5,4,[15,16])
-        imshow(reshape(c_L{2}(:,ii),13,20))
-        caxis([-1.5,1.5])
-        colorbar
-        subplot(5,4,[17,18])
-        imshow(reshape(c_L{3}(:,ii),13,20))
-        caxis([-1.5,1.5])
-        colorbar
-        subplot(5,4,[19,20])
-        imshow(reshape(c_L{4}(:,ii),13,20))  
-        caxis([-1.5,1.5])
-        colormap jet
-        colorbar
-        
-        color_vec = colormap;
-        subplot(5,4,[1:3,5:7,9:11])
-        plot(Xrecon(ii, :)- Xrecon(1, 1), Yrecon(ii, :) - Yrecon(1, 1), 'Color', color_vec(round(255*((neu_val+3)/6)),:),'LineWidth',3)
-        hold on
-        plot(Xrecon(ii,1)- Xrecon(1, 1), Yrecon(ii, 1) - Yrecon(1, 1), '.', 'Color', 'b', 'MarkerSize', 20)
-        axis equal
-        xlim([ min(min(Xrecon - Xrecon(1, 1))), max(max(Xrecon - Xrecon(1, 1)))])
-        ylim([ min(min(Yrecon - Yrecon(1, 1))), max(max(Yrecon - Yrecon(1, 1)))])
-        hold off
-        
-        cc1=['Index of the neuron is (',num2str(idx),')'];
-        text(460,20,cc1);
-        neu_val = c_L{idx(1)}((idx(1)-1)*260+idx(2),ii);
-        cc2=['The neuron value is: ',num2str(neu_val)];
-        text(460,800,cc2); 
-        subplot(5,4,8)
-        imshow(c_L{idx(1)}((idx(1)-1)*260+idx(2),ii))
-        caxis([-3,3])
-        colormap jet
-        
-      %  pause(0.01) % forces refresh so you can see the plot
-    end
-    
+%     %% plot a movie of worm with motion back in
+%     for ii = 4100:1:4700   %size(Xrecon,1) % only plotting every 10 frames to speed up movie
+% %   
+% %         subplot(5,4,[13,14])
+% %         imshow(reshape(c_L{1}(:,ii),13,20))
+% %         caxis([-3,3])
+% %         colorbar
+% %         subplot(5,4,[15,16])
+% %         imshow(reshape(c_L{2}(:,ii),13,20))
+% %         caxis([-1.5,1.5])
+% %       %  colorbar
+% %         subplot(5,4,[17,18])
+% %         imshow(reshape(c_L{3}(:,ii),13,20))
+% %         caxis([-1.5,1.5])
+% %       %  colorbar
+% %         subplot(5,4,[19,20])
+% %         imshow(reshape(c_L{4}(:,ii),13,20))  
+% %         caxis([-1.5,1.5])
+%          colormap jet
+% %        % colorbar
+%         
+% %         hp4 = get(subplot(5,4,20),'Position');
+% %         colorbar('Position', [hp4(1)+hp4(3)+0.01  hp4(2)  0.1  hp4(2)+hp4(3)*1.1])
+%         
+%         color_vec = colormap;
+%         subplot(5,4,[1:3,5:7,9:11])
+%         neu_val = min(3,max(-2.98,c_L{idx(1)}((idx(1)-1)*260+idx(2),ii)));
+%         plot(Xrecon(ii, :)- Xrecon(1, 1), Yrecon(ii, :) - Yrecon(1, 1), 'Color', color_vec(round(255*((neu_val+3)/6)),:),'LineWidth',3)
+%         hold on
+%         plot(Xrecon(ii,1)- Xrecon(1, 1), Yrecon(ii, 1) - Yrecon(1, 1), '.', 'Color', 'b', 'MarkerSize', 20)
+%         axis equal
+%         xlim([ min(min(Xrecon - Xrecon(1, 1))), max(max(Xrecon - Xrecon(1, 1)))])
+%         ylim([ min(min(Yrecon - Yrecon(1, 1))), max(max(Yrecon - Yrecon(1, 1)))])
+%         hold off
+%         
+%         fea_val = fea_timeseries_show(idx_feature,ii);
+%         cc0 = ['The feature''s value is: ',num2str(fea_val)];
+%         text(460,950,cc0);
+%         cc1=['Index of the neuron is (',num2str(idx),')'];
+%         text(460,20,cc1);
+%         cc2=['The neuron''c value is: ',num2str(neu_val)];
+%         text(460,800,cc2); 
+%         subplot(5,4,8)
+%         imshow(c_L{idx(1)}((idx(1)-1)*260+idx(2),ii))
+%         caxis([-3,3])
+%         colormap jet
+%         
+%         pause(0.00000000001) % forces refresh so you can see the plot
+%      end
+
+end 
+     
  %% find correlation to features
  fea_info = h5info([cur_folder,feature_name]); 
  fea_timeseries = h5read([cur_folder,feature_name],'/features_timeseries'); 
  SNames = fieldnames(fea_timeseries);
  
+%  midbody_bends_length = length(worm.locomotion.bends.midbody.amplitude);
+%  midbody_bends = worm.locomotion.bends.midbody.amplitude(round(0.1*midbody_bends_length):3:end);
  
- 
+ fea_timeseries_all = zeros(length(SNames),max(size(fea_timeseries.(SNames{1}))));
+ for ii = 1:length(SNames);
+     fea_timeseries_all(ii,:) = fea_timeseries.(SNames{ii});
+ end
+ subsample_step = 3;
+ fea_timeseries_subs = fea_timeseries_all(:,1:subsample_step:end);
+ idx_timeseries = size(fea_timeseries_subs,2)- size(c_L{1},2)+1;
+ fea_timeseries_show =  fea_timeseries_subs(:,idx_timeseries:end);
+ row_num = size(fea_timeseries_show,1);
+ lay_num = size(c_L,1);
+ idx_num = size(c_L{1},1);
+ col_num = lay_num*idx_num;
+ corr_mtx = zeros(row_num,col_num);
+%  for ii = 1:row_num
+%      for jj = 1:lay_num
+%          for kk = 1:idx_num
+%             corr_mtx(ii,(jj-1)*idx_num+kk) =xcorr( fea_timeseries_show(ii,:),c_L{jj,1}(kk,:));
+%          end
+%      end
+%  end
+comb_mtx = cell(4,1);
+comb_mtx_NaN0 = cell(4,1);
+R = cell(4,1);
+R_largeEntry = cell(4,1);
+for ii = 1:4;
+    comb_mtx{ii} = [fea_timeseries_show;c_L{ii,1}];
+    comb_mtx_NaN0{ii} = comb_mtx{ii};
+    comb_mtx_NaN0{ii}(isnan(comb_mtx_NaN0{ii}))=0;
+    
+    R{ii} = corrcoef(comb_mtx_NaN0{ii}');
+    R_largeEntry{ii} = zeros(size(R{ii}));
+    R_largeEntry{ii}(abs(R{ii})>0.6) = R{ii}(abs(R{ii})>0.6);
+  %  R_largeEntry{ii}(1:56,1:56)=0;
+  %  R_largeEntry{ii}(57:end,57:end)=0;
+    
+    for jj = 1:size(comb_mtx_NaN0{ii},1)
+        comb_mtx_NaN0{ii}(jj,:) = comb_mtx_NaN0{ii}(jj,:)-mean(comb_mtx_NaN0{ii}(jj,:));
+        comb_mtx_NaN0{ii}(jj,:) = comb_mtx_NaN0{ii}(jj,:)/std(comb_mtx_NaN0{ii}(jj,:));
+    end
 end
+
+comb_mtx2 = cell(4,1);
+comb_mtx2_NaN0 = cell(4,1);
+R2 = cell(4,1);
+R2_largeEntry = cell(4,1);
+for ii = 1:4;
+    comb_mtx2{ii} = [fea_timeseries_show;res_L{ii,1}];
+    comb_mtx2_NaN0{ii} = comb_mtx2{ii};
+    comb_mtx2_NaN0{ii}(isnan(comb_mtx2_NaN0{ii}))=0;
+    
+    R2{ii} = corrcoef(comb_mtx2_NaN0{ii}');
+    R2_largeEntry{ii} = zeros(size(R2{ii}));
+    R2_largeEntry{ii}(abs(R2{ii})>0.6) = R2{ii}(abs(R2{ii})>0.6);
+  %  R_largeEntry{ii}(1:56,1:56)=0;
+  %  R_largeEntry{ii}(57:end,57:end)=0;
+    
+    for jj = 1:size(comb_mtx2_NaN0{ii},1)
+        comb_mtx2_NaN0{ii}(jj,:) = comb_mtx2_NaN0{ii}(jj,:)-mean(comb_mtx2_NaN0{ii}(jj,:));
+        comb_mtx2_NaN0{ii}(jj,:) = comb_mtx2_NaN0{ii}(jj,:)/std(comb_mtx2_NaN0{ii}(jj,:));
+    end
+end
+
+% for ii =1:56;
+%     figure, plot(comb_mtx_NaN0{1,1}(57,:),'r'), hold on, plot(comb_mtx_NaN0{1,1}(ii,:))
+% end
